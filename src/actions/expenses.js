51a04bdbc9b575b03b487045e8116c76b6741ea6
Daniel Pereira <danielpersilva@gmail.com>
2024-01-10 09:47:39 +0000

    Remove expense from Firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -51,6 +51,15 @@ export const removeExpense = ({ id } = {}) => ({
     id
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense({ id }));
+        });
+    };
+};
+
+
 //EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
@@ -79,3 +88,5 @@ export const startSetExpenses = () => {
         });
     };
 };
+
+
