5e446d12c1d7b58f4c15fc55284b2d29d3fb361b
Daniel Pereira <danielpersilva@gmail.com>
2024-01-11 09:18:19 +0000

    Create a Login Page and login with GoogleProvider

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import 'react-dates/initialize';
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 
 const store = configureStore();
 
@@ -21,6 +21,14 @@ const jsx = (
 
 // createRoot(<p>Loading ...</p>).render(jsx);
 
-store.dispatch(startSetExpenses()).then(()=> {
+store.dispatch(startSetExpenses()).then(() => {
     createRoot(document.getElementById('app')).render(jsx);
 });
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        console.log('log in');
+    } else {
+        console.log('log out');
+    }
+})
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,18 +4,19 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import ExpensiveDashboardPage from '../components/ExpensiveDashboardPage';
-import HelpPage from  '../components/HelpPage';
+import HelpPage from '../components/HelpPage';
 import Header from "../components/Header";
-import NotFoundPage from  '../components/NotFoundPage';
-
+import NotFoundPage from '../components/NotFoundPage';
+import LoginPage from "../components/LoginPage";
 
 const AppRouter = () => (
     <BrowserRouter>
         <div>
             <Header />
             <Switch>
-                <Route path="/" component={ExpensiveDashboardPage} exact={true} />
+                <Route path="/" component={LoginPage} exact={true} />
                 <Route path="/create" component={AddExpensePage} />
+                <Route path='/dashboard' component={ExpensiveDashboardPage} />
                 <Route path="/edit/:id" component={EditExpensePage} />
                 <Route path="/help" component={HelpPage} />
                 <Route component={NotFoundPage} />
